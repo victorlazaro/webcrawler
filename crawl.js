04183be07a7dc7cdbf849aec658a77d5b7e3c4b2
Victor Lazaro <victorlazaro@Victors-MacBook-Air.local>
2023-02-06 20:16:35 -0700

    Crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,26 +1,47 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrURL = normalizeURL(currentURL)
+
+    if (pages[normalizedCurrURL] > 0 ) {
+        pages[normalizedCurrURL]++
+        return pages
+    }
+
+    pages[normalizedCurrURL] = 1
+
     console.log(`actively crawling ${currentURL}`)
 
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`non html response, contentType: ${contentType} on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        nextURLs = getURLsFromHTML(htmlBody, baseURL)
+        
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch(err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
-
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {

692950cc3204a33d1cbbe222d08b861472f60142
Victor Lazaro <victorlazaro@Victors-MacBook-Air.local>
2023-02-06 20:07:07 -0700

    Crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    }
+    else if (process.argv.length > 3) {
+        console.log("too many args")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+
+main()

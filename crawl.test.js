cbbd5731825c2392a7d20d0f5a67cabb865ba0a5
Victor Lazaro <victorlazaro@Victors-MacBook-Air.local>
2023-02-06 19:27:49 -0700

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL strips protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL strip http', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
